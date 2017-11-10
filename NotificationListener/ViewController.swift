cfc93764850f6d713d35d75131391f09423e64d5
Björn Fröhling <bjorn.frohling@qvik.fi>
2017-11-10 06:02:20 +0200

    Initial Commit

---
+++ NotificationListener/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationListener
+//
+//  Created by Björn Fröhling on 10/11/2017.
+//  Copyright © 2017 Frohling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
